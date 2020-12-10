118d9d95f674e816a7734de58156c0b3dae9d8c1
Chudier <chudierp@gmail.com>
2020-12-10 16:36:38 -0700

    completed, starting challenges

---
+++ src/Heading.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Heading.css';
+
+function Title() {
+  return (
+    <div className="Title">
+      <ul>
+        <li>FEW 1.2 Final Assessment</li>
+        <li>Chudier Chuol</li>
+    </ul>
+    </div>
+  )
+}
+
+export default Title
